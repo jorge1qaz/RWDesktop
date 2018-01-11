9fbe55f11ac9d9b3bf42149403dbc16be1c8f8d4
jorgeluis <jorgealcantara5a@gmail.com>
2018-01-11 18:43:49 -0500

    Estados financieros

--- BusinessLayer/R_EstadosFinancieros.cs
+++ BusinessLayer/R_EstadosFinancieros.cs
@@ -45,10 +45,20 @@ namespace BusinessLayer
             ExportTable(pathSaveFile, pathConnection, "A550", "CCOD_BAL2", "A550", "CCOD_BALN2", true); ExportTable(pathSaveFile, pathConnection, "A560", "CCOD_BAL2", "A560", "CCOD_BALN2", true);
             ExportTable(pathSaveFile, pathConnection, "A570", "CCOD_BAL2", "A570", "CCOD_BALN2", true); ExportTable(pathSaveFile, pathConnection, "A575", "CCOD_BAL2", "A575", "CCOD_BALN2", true);
             ExportTable(pathSaveFile, pathConnection, "A580", "CCOD_BAL2", "A580", "CCOD_BALN2", true); ExportTable(pathSaveFile, pathConnection, "A610", "CCOD_BAL2", "A610", "CCOD_BALN2", true);
-            ExportTable(pathSaveFile, pathConnection, "A630", "CCOD_BAL2", "A630", "CCOD_BALN2", true);
-
-            ExportTable(pathSaveFile, pathConnection, "A105", "CCOD_BAL2", true);
-
+            ExportTable(pathSaveFile, pathConnection, "A630", "CCOD_BAL2", "A630", "CCOD_BALN2", true); ExportTable(pathSaveFile, pathConnection, "P105", "CCOD_BAL2", "P105", "CCOD_BALN2", false);
+            ExportTable(pathSaveFile, pathConnection, "P110", "CCOD_BAL2", "P110", "CCOD_BALN2", false); ExportTable(pathSaveFile, pathConnection, "P120", "CCOD_BAL2", "P120", "CCOD_BALN2", false);
+            ExportTable(pathSaveFile, pathConnection, "P121", "CCOD_BAL2", "P121", "CCOD_BALN2", false); ExportTable(pathSaveFile, pathConnection, "P123", "CCOD_BAL2", "P123", "CCOD_BALN2", false);
+            ExportTable(pathSaveFile, pathConnection, "P125", "CCOD_BAL2", "P125", "CCOD_BALN2", false); ExportTable(pathSaveFile, pathConnection, "P130", "CCOD_BAL2", "P130", "CCOD_BALN2", false);
+            ExportTable(pathSaveFile, pathConnection, "P135", "CCOD_BAL2", "P135", "CCOD_BALN2", false); ExportTable(pathSaveFile, pathConnection, "P137", "CCOD_BAL2", "P137", "CCOD_BALN2", false);
+            ExportTable(pathSaveFile, pathConnection, "P210", "CCOD_BAL2", "P210", "CCOD_BALN2", false); ExportTable(pathSaveFile, pathConnection, "P410", "CCOD_BAL2", "P410", "CCOD_BALN2", false);
+            ExportTable(pathSaveFile, pathConnection, "P415", "CCOD_BAL2", "P415", "CCOD_BALN2", false); ExportTable(pathSaveFile, pathConnection, "P420", "CCOD_BAL2", "P420", "CCOD_BALN2", false);
+            ExportTable(pathSaveFile, pathConnection, "P425", "CCOD_BAL2", "P425", "CCOD_BALN2", false); ExportTable(pathSaveFile, pathConnection, "P430", "CCOD_BAL2", "P430", "CCOD_BALN2", false);
+            ExportTable(pathSaveFile, pathConnection, "P435", "CCOD_BAL2", "P435", "CCOD_BALN2", false); ExportTable(pathSaveFile, pathConnection, "P440", "CCOD_BAL2", "P440", "CCOD_BALN2", false);
+            ExportTable(pathSaveFile, pathConnection, "P445", "CCOD_BAL2", "P445", "CCOD_BALN2", false); ExportTable(pathSaveFile, pathConnection, "P450", "CCOD_BAL2", "P450", "CCOD_BALN2", false);
+            ExportTable(pathSaveFile, pathConnection, "P805", "CCOD_BAL2", "P805", "CCOD_BALN2", false); ExportTable(pathSaveFile, pathConnection, "P810", "CCOD_BAL2", "P810", "CCOD_BALN2", false);
+            ExportTable(pathSaveFile, pathConnection, "P815", "CCOD_BAL2", "P815", "CCOD_BALN2", false); ExportTable(pathSaveFile, pathConnection, "P820", "CCOD_BAL2", "P820", "CCOD_BALN2", false);
+            ExportTable(pathSaveFile, pathConnection, "P830", "CCOD_BAL2", "P830", "CCOD_BALN2", false); ExportTable(pathSaveFile, pathConnection, "P835", "CCOD_BAL2", "P835", "CCOD_BALN2", false);
+            ExportTable(pathSaveFile, pathConnection, "P840", "CCOD_BAL2", "P840", "CCOD_BALN2", false);
 
         }
         //Jorge Luis|09/01/2018|RW-109
